4351f65577d2bea7e49c5b5151e16f6c9a6571f3
907badhon <badhonislam891@gmail.com>
2026-01-12 22:53:15 +0600

    Updated API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost/ecommerce-backend/api";
+const API_BASE_URL = "https://e-shopbd.lovestoblog.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
